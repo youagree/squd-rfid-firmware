5f743fca25e015c0cef0cf28d55a499f70be7055
Eugene_Sleptsov <infralighter@gmail.com>
2021-02-24 23:08:27 +0300

    add Serial read

--- sketch_feb01a/sketch_feb01a.ino
+++ sketch_feb01a/sketch_feb01a.ino
@@ -6,8 +6,6 @@ byte ip[] = { 10, 189, 113, 200 };
 
 char server[] = "10.189.113.254";
 
-int count = 0;
-
 EthernetClient client;
 
 
@@ -20,7 +18,7 @@ void setup() {
   Serial.println("connecting...");
 
   // от старшего к младшему: a[0] - старший, a[N] - младший
-  uint8_t a[] = {104, 253, 254};
+  //  uint8_t a[] = {104, 253, 254};
 
   //   {01101000, 11111111, 11111111}
   //a0  01101000  00000000  00000000 
@@ -29,7 +27,7 @@ void setup() {
 
   //res 01101000  11111101  11111110
 
-  Serial.println(byte_array_to_decimal(a));
+  //Serial.println(byte_array_to_decimal(a));
 } 
 
 
@@ -54,14 +52,9 @@ uint32_t byte_array_to_decimal(byte* arr) {
     for (int i = 0; i < s; i++) {
       result = result | arr[i];
       result = result << 8;
-      Serial.print(i);
-      Serial.print(": ");
-      Serial.println(result);
     }
 
     result = result | arr[s];
-
-    Serial.println(result);
 
     return result;
 }
@@ -70,36 +63,43 @@ uint32_t byte_array_to_decimal(byte* arr) {
 void loop() {
   // здесь считывание с шины событий rfid (id устройства и непосредственно rfid метка)
 
-  char a[3] = {0x15, 0x36, 0x37};
-  
-  // отправка события на сервер
-  send_event(((++count) & 255), a);
+  uint8_t address = 0;
+  uint8_t rfid[3];
 
-  String response;
-
-  delay(25);
-  // вычитывание кода ответа (1 строка ответа содержит http код ответа)
-  if (client.available()) {
-    response = client.readStringUntil('\r');
-  }
-
-  // если 200, вычитываем ответ до конца
-  // последняя строка ответа в положительносм сценарии - это id шлагбаума который нужно открыть
-  if (response.charAt(9) == '2') {
-      while(client.available()) {
-          response = client.readStringUntil('\r');
+  if (Serial.available() > 0) {
+    if (Serial.read() == ':') {
+      address = Serial.read();
+      for (int i = 0; i < 3; i++) {
+        rfid[i] = Serial.read();
       }
 
-      // send signal to шлагбаум
-      Serial.println(response);
-  }
-  else {
-    Serial.println("not 200");
-    Serial.println(response);
-  }
+      // отправка события на сервер
+      send_event(address, rfid);
 
- // если не 200, продолжаем слушать шину
- delay(1000);
+      String response;
+    
+      delay(25);
+      // вычитывание кода ответа (1 строка ответа содержит http код ответа)
+      if (client.available()) {
+        response = client.readStringUntil('\r');
+      }
+    
+      // если 200, вычитываем ответ до конца
+      // последняя строка ответа в положительносм сценарии - это id шлагбаума который нужно открыть
+      if (response.charAt(9) == '2') {
+          while(client.available()) {
+              response = client.readStringUntil('\r');
+          }
+    
+          // send signal to шлагбаум
+          Serial.println(response);
+      }
+      else {
+        Serial.println("not 200");
+        Serial.println(response);
+      }
+    }
+  }
 }
 
 /** 
